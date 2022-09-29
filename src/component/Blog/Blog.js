346944fe6f64f351b51497eee4f53ca600a533ff
Sumanta62 <sumantamajumder62@gmail.com>
2022-09-29 17:00:25 +0600

    Second question add

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -17,7 +17,7 @@ const Blog = () => {
                     <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne"
                         data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body">
-                            <p><b>Ans:</b> Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components </p>
+                            <p><b>Ans:</b>React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes. </p>
                         </div>
                     </div>
                 </div>
